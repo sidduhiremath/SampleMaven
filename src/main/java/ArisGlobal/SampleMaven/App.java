92d07fd27d213d6e750013bc045404e42fb3e993
Siddalingayya Hiremath <siddalingayya.hiremath@arisglobal.com>
2021-10-08 12:57:13 +0530

    First Commit

---
+++ src/main/java/ArisGlobal/SampleMaven/App.java
@@ -0,0 +1,13 @@
+package ArisGlobal.SampleMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/ArisGlobal/SampleMaven/Sample.java
@@ -0,0 +1,10 @@
+package ArisGlobal.SampleMaven;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Heelo");
+	}
+
+}
